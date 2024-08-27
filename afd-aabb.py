2a4f14615a212c76165c08fd0662a6e8685e25e5
aojoaojo <aojoaojov@gmail.com>
2024-08-27 09:18:55 -0300

    Atividade AFD checando pares de letras de uma palavra no alfabeto {a, b}

---
+++ afd-aabb.py
@@ -0,0 +1,47 @@
+def estado1(indice_letra, palavra):
+    print('Estado 1')
+    if indice_letra == len(palavra):
+        print('Palavra aceita')
+        return True
+    elif palavra[indice_letra] == 'a':
+        return estado2(indice_letra+1, palavra)
+    elif palavra[indice_letra] == 'b':
+        return estado4(indice_letra+1, palavra)
+
+def estado2(indice_letra, palavra):
+    print('Estado 2')
+    if indice_letra == len(palavra):
+        print('Palavra negada')
+        return False
+    elif palavra[indice_letra] == 'b':
+        return estado3(indice_letra+1, palavra)
+    elif palavra[indice_letra] == 'a':
+        return estado1(indice_letra+1, palavra)
+
+    
+def estado3(indice_letra, palavra):
+    print('Estado 3')
+    if indice_letra == len(palavra):    
+        print('Palavra negada')
+        return False
+    elif palavra[indice_letra] == 'b':
+        return estado2(indice_letra+1, palavra)
+    elif palavra[indice_letra] == 'a':
+        return estado4(indice_letra+1, palavra)
+
+def estado4(indice_letra, palavra):
+    print('Estado 4')
+    if indice_letra == len(palavra):    
+        print('Palavra negada')
+        return False
+    elif palavra[indice_letra] == 'a':
+        return estado3(indice_letra+1, palavra)
+    elif palavra[indice_letra] == 'b':
+        return estado1(indice_letra+1, palavra)
+
+def main():
+    palavra = input('Insira uma palavra do alfaberto {a, b}: ')
+    estado1(0, palavra)
+
+if __name__ == '__main__':
+    main()
